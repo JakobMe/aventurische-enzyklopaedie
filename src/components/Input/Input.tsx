bea2ae8ae11b15e1ff4ae35d8dbd9b896f0987a6
Jakob Metzger <jakob.me@gmail.com>
2018-01-30 23:48:42 +0100

    feat: Adds optional class to Input component.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,11 +5,13 @@ interface InputProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
+  className?: string;
 }
 
 export default class Input extends React.Component<InputProps> {
   public static defaultProps: Partial<InputProps> = {
-    placeholder: '...'
+    placeholder: '',
+    className: ''
   };
 
   handleOnChange = ($event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,7 +21,7 @@ export default class Input extends React.Component<InputProps> {
   render() {
     return (
       <input
-        className="Input"
+        className={`Input ${this.props.className}`}
         value={this.props.value}
         onChange={this.handleOnChange}
         placeholder={this.props.placeholder}
