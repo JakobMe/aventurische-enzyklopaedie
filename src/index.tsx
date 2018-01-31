0418094d6c439f0cba372574ec0198006a8f15be
Jakob Metzger <jakob.me@gmail.com>
2018-01-31 22:18:09 +0100

    refactor: Changes fuseOptions to json.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import * as ReactDOM from 'react-dom';
 import * as Fuse from 'fuse.js';
 import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App/App';
-import fuseOptions from './config/fuseOptions';
 import wikiData from './data/wikiData';
 
 ReactDOM.render(
-  <App fuse={new Fuse(wikiData, fuseOptions)} data={wikiData} />,
+  <App
+    fuse={new Fuse(wikiData, require('./config/fuseOptions.json'))}
+    data={wikiData}
+  />,
   document.getElementById('Root') as HTMLElement
 );
 registerServiceWorker();
